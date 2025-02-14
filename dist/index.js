0495a04e903d2bc647a0160bcbb9e7e9dd11e8aa
superNeigh <superneighbor2024@gmail.com>
2025-02-14 17:47:50 +0100

    Updated CORS configuration to use client URL from environment variables

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const server = (0, http_1.createServer)(app);
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST"],
 }));
